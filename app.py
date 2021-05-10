7a6311c21868ef60105ae3ad505616e93890d775
Siddhartha Roy <sid86harth@gmail.com>
2021-05-11 05:02:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,168 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from flask_mail import Mail
+import os
+import math
+import json
+
+
+with open('config.json', 'r') as f:
+    params = json.load(f)["params"]
+
+app = Flask(__name__)
+app.secret_key = 'my-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///codingthrottlerz.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = params['upload_location']
+db = SQLAlchemy(app)
+mail = Mail(app)
+
+
+class Contacts(db.Model):
+    slno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    sub_title = db.Column(db.String(120), nullable=False)
+    slug = db.Column(db.String(25), nullable=False)
+    content = db.Column(db.String(400), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    img_file = db.Column(db.String(15), nullable=False)
+
+
+@app.route('/')
+def home():
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
+    if page==1:
+        prev = "#"
+        next = "/?page="+ str(page+1)
+    elif page==last:
+        prev = "/?page="+ str(page-1)
+        next = "#"
+    else:
+        prev = "/?page="+ str(page-1)
+        next = "/?page="+ str(page+1)
+    
+    return render_template('home.html', params=params, posts=posts, prev=prev, next=next)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', params=params)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/dashboard')
+
+
+@app.route('/delete/<string:sno>', methods=['GET', 'POST'])
+def delete(sno):
+    if ('user' in session and session['user'] == params['admin_user']):
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if ('user' in session and session['user'] == params['admin_user']):
+        if request.method == 'POST':
+            f = request.files['file1']
+            f.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return redirect('/dashboard')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    if ('user' in session and session['user'] == params['admin_user']):
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params, posts=posts)
+
+    if request.method == 'POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if (username == params['admin_user'] and userpass == params['admin_pass']):
+            # set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts=posts)
+
+    return render_template('log.html', params=params)
+
+
+@app.route('/post/<string:post_slug>', methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+
+    if (request.method == 'POST'):
+
+        # add entry to the database
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, email=email, phone_num=phone,
+                         date=datetime.now(), message=message)
+        db.session.add(entry)
+        db.session.commit()
+        return render_template('submit.html', params=params)
+    return render_template('contact.html', params=params)
+
+
+@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
+def edit(sno):
+    if 'user' in session and session['user'] == params['admin_user']:
+        if request.method == 'POST':
+            box_title = request.form.get('title')
+            box_sub_title = request.form.get('sub_title')
+            box_slug = request.form.get('slug')
+            box_content = request.form.get('content')
+            box_img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno == '0':
+                post = Posts(title=box_title, slug=box_slug, content=box_content,
+                             sub_title=box_sub_title, img_file=box_img_file, date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
+                post = Posts.query.filter_by(sno=sno).first()
+                post.title = box_title
+                post.sub_title = box_sub_title
+                post.slug = box_slug
+                post.content = box_content
+                post.img_file = box_img_file
+                post.date = date
+                db.session.commit()
+                return redirect('/edit/'+sno)
+
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, post=post, sno=sno)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
